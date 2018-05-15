65550f85d8fbc51bed785d01a850f392fd591969
Tim Brackfield <Cloud_193@hotmail.com>
2018-05-15 17:06:54 +0100

    created ugly articles app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+include Faker
+
+10.times do
+  Article.create(
+    title: Faker::Book.title,
+    content: Faker::Company.catch_phrase
+    )
+end
